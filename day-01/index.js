6befe3a50e5f4f470d550a32f367d2bfa226b2db
VlatkovicB <vlatkovicbojan@gmail.com>
2024-12-28 14:14:38 +0100

    Day 01 - Part Two

--- day-01/index.js
+++ day-01/index.js
@@ -1,6 +1,6 @@
 import { readData } from "../utils.js"
 
-const solving = () => {
+const partOne = () => {
   const input = readData("./day-01/input.txt")
   const { left, right } = input.reduce(
     (acc, curr) => {
@@ -23,4 +23,33 @@ const solving = () => {
   }, 0)
 }
 
-console.log(solving())
+const partTwo = () => {
+  const input = readData("./day-01/input.txt")
+  const { left, right } = input.reduce(
+    (acc, curr) => {
+      const [left, right] = curr.split(/ +/g)
+      acc.left.push(Number(left))
+      acc.right.push(Number(right))
+      return acc
+    },
+    { left: [], right: [] }
+  )
+
+  const rightSideMap = right.reduce((a, c) => {
+    if (a[c]) {
+      a[c] += 1
+    } else {
+      a[c] = 1
+    }
+    return a
+  }, {})
+
+  return left.reduce((a, c) => {
+    const rightSideNumber = Number(rightSideMap[c]) || 0
+    a += c * rightSideNumber
+
+    return a
+  }, 0)
+}
+
+console.log(partTwo())
